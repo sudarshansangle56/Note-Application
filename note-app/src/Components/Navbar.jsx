90b9c9bf456984b8de85bde14f0e11aa523bb76f
sudarshan sangle <darshsangle9456@gmail.com>
2025-08-28 20:07:50 +0530

    Navbar done almost

--- note-app/src/Components/Navbar.jsx
+++ note-app/src/Components/Navbar.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
-
+import React from "react";
+import Logo from "../Components/Logo";
 function Navbar() {
   return (
-    <div>
-      
+    <div className="h-[60px] flex items-center justify-between px-5 ">
+      <div className="flex gap-2 ">
+        <Logo />
+        <h1 className="font-semibold text-[25px]">Dashboard</h1>
+      </div>
+      <h1 className="text-[#6760eb] font-semibold border-b-2 border-[#7d52e1]">Sign Out</h1>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
