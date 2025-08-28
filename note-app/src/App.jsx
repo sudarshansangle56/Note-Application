ba2a75ce59a21270b9088fa99965f66d764e7366
sudarshan sangle <darshsangle9456@gmail.com>
2025-08-28 18:39:07 +0530

    project strucutre is ready

---
+++ note-app/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+ 
+  return (
+    <div className="h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">
+        My Note App 📒
+      </h1>
+    </div>
+  )
+}
+
+export default App
