db2a91a09c98bc3a0b257d9df258a0da3396b404
sudarshan sangle <darshsangle9456@gmail.com>
2025-08-31 20:01:57 +0530

    dashboard almost done

--- note-app/src/pages/Dashboard.jsx
+++ note-app/src/pages/Dashboard.jsx
@@ -1,26 +1,39 @@
-import React from 'react'
-import Logo from '../Components/Logo'
+import React from "react";
+import { Trash2 } from "lucide-react";
+import Logo from "../Components/Logo";
+import Navbar from "../Components/Navbar";
 
 function Dashboard() {
   return (
-    <div>
-      <div className='border-2 border-[#222222a7] rounded-xl p-7'>
-        <h1 className='font-bold'>Welcome , Jonas Kahnwald ! </h1>
-        <p>Email: xxxx@xxx.com</p>
+    <div className="px-4">
+      <Navbar />
+      <div className="border-2 border-[#222222a7] rounded-xl p-7 mt-4">
+        <h1 className="font-bold text-lg sm:text-xl">
+          Welcome , Jonas Kahnwald !
+        </h1>
+        <p className="text-sm sm:text-base">Email: xxxx@xxx.com</p>
       </div>
-      <div className='h-[45px] mt-5 w-[500px] bg-[#dbdbdb] p-5 flex items-center justify-between rounded-lg'>
-        <h1>Note 1</h1>
-         delete button
+
+      <div className="mt-5 space-y-4 max-w-lg">
+        <div className="flex items-center justify-between bg-[#dbdbdb] rounded-lg px-4 py-2">
+          <h1 className="text-sm sm:text-base">Note 1</h1>
+          <button className="text-black hover:text-red-800">
+            <Trash2 size={20} />
+          </button>
+        </div>
+        <div className="flex items-center justify-between bg-[#dbdbdb] rounded-lg px-4 py-2">
+          <h1 className="text-sm sm:text-base">Note 2</h1>
+          <button className="text-black hover:text-red-800">
+            <Trash2 size={20} />
+          </button>
+        </div>
       </div>
-      <div className='h-[45px] mt-5 w-[500px] bg-[#dbdbdb] p-5 flex items-center justify-between rounded-lg'>
-        <h1>Note 1</h1>
-         delete button
-      </div>
-      <button className='w-[95%] h-[50px] rounded-lg text-white bg-blue-700 mt-9'>
+
+      <button className="w-full sm:w-[95%] h-[50px] rounded-lg text-white bg-blue-700 mt-9">
         Create Note
       </button>
     </div>
-  )
+  );
 }
 
-export default Dashboard
+export default Dashboard;
